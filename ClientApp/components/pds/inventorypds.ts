9db486ba2eb0e56d8687b6ff6d3556bdbc9da444
Jake Gough <super.jake@hotmail.co.uk>
2018-09-28 08:58:07 +0100

    Update inventorypds.ts

--- ClientApp/components/pds/inventorypds.ts
+++ ClientApp/components/pds/inventorypds.ts
@@ -1,19 +1,27 @@
 import Vue from 'vue';
 import { Component } from 'vue-property-decorator';
 import { Reagent } from '../../models/reagent';
+import { Stock } from '../../models/stock';
 
 @Component
 export default class InventoryPDSComponent extends Vue {
 	reagents: Reagent[] = [];
+	stocks: Stock[] = [];
+	filtered: Reagent[] = [];
 	loading: boolean = false;
-	search: string = "";
-	headers: object[] = [
-		{ text: 'Name', value: 'name' },
+	reagentSearch: string = "";
+	inventorySearch: string = "";
+	reagentHeaders: object[] = [
+		{ text: 'Name', value: 'name', sortable: false },
+		{ text: 'Minimum', value: 'minimum', sortable: false },
+		{ text: 'Quantity', value: 'quantity', sortable: false }
+	];
+	inventoryHeaders: object[] = [
+		{ text: 'Name', value: 'name', sortable: false },
 		{ text: 'Supplier', value: 'supplier' },
 		{ text: 'Batch', value: 'batch' },
 		{ text: 'Validated', value: 'validated' },
 		{ text: 'Expiry', value: 'expiry' },
-		{ text: 'Minimum', value: 'minimum' },
 		{ text: 'Quantity', value: 'quantity' }
 	];
 
@@ -24,11 +32,21 @@ export default class InventoryPDSComponent extends Vue {
 	loadInventory() {
 		this.loading = true;
 		fetch('api/PDS/GetInventory')
-			.then(response => response.json() as Promise<Reagent[]>)
+			.then(response => response.json())
 			.then(data => {
-				this.reagents = data;
+				this.reagents = data["item1"];
+				this.stocks = data["item2"];
 				this.loading = false;
-			});
+			})
+	}
+
+	filterReagents(name: string) {
+		this.filtered = [];
+		for (var i = 0; i < this.reagents.length; i++) {
+			if (this.reagents[i].name === name) {
+				this.filtered.push(this.reagents[i]);
+			}
+		}
 	}
 
 	warningColour(warning: number) {
